2bf716a49b21cda2a629279b6b3ea782351e8564
Vladimir <57994892+wladik06@users.noreply.github.com>
2022-12-31 20:04:07 +0100

    implemented authentication (signup+login)

---
+++ server/src/v1/routes/auth.js
@@ -0,0 +1,46 @@
+const router = require("express").Router();
+const userController = require("../controllers/userController");
+const { body } = require("express-validator");
+const validation = require("../handlers/validationHanlder");
+const tokenHandler = require("../handlers/tokenHandler");
+const User = require("../models/user");
+
+router.post(
+	"/signup",
+	body("username")
+		.isLength({ min: 8 })
+		.withMessage("Username must contain at least 8 characters"),
+	body("password")
+		.isLength({ min: 8 })
+		.withMessage("Password must contain at least 8 characters"),
+	body("confirmPassword")
+		.isLength({ min: 8 })
+		.withMessage("Password must be at least 8 characters"),
+	body("username").custom((value) => {
+		return User.findOne({ username: value }).then((user) => {
+			if (user) {
+				return Promise.reject("Username already in use");
+			}
+		});
+	}),
+	validation.validate,
+	userController.register
+);
+
+router.post(
+	"/login",
+	body("username")
+		.isLength({ min: 8 })
+		.withMessage("username must contain at least 8 characters"),
+	body("password")
+		.isLength({ min: 8 })
+		.withMessage("password must contain at least 8 characters"),
+	validation.validate,
+	userController.login
+);
+
+router.post("/verify-token", tokenHandler.verifyToken, (req, res) => {
+	res.status(200).json({ user: req.user });
+});
+
+module.exports = router;

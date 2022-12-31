18983dbd7079e204a605099e509e785c869ba8d1
Vladimir <57994892+wladik06@users.noreply.github.com>
2022-12-31 22:52:10 +0100

    axios config final

--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -1,7 +1,24 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import authUtils from "../../utils/authUtils";
+
 
 const AppLayout = () => {
+	const navigate = useNavigate();
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const checkAuth = async () => {
+			const isAuth = await authUtils.isAuthenticated();
+			if (!isAuth) {
+				setLoading(false);
+			} else {
+				navigate("/");
+			}
+		};
+		checkAuth();
+	}, [navigate]);
+
 	return (
 		<div>
 			<Outlet />

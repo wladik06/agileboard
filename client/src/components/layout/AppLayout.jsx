533d94b6c21e6b2a23ef0e4dd495acd69811a063
Vladimir <57994892+wladik06@users.noreply.github.com>
2022-12-31 23:23:45 +0100

    checkAuth redirect

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,12 +1,47 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Box } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import authUtils from "../../utils/authUtils";
+import Loading from "../common/Loading";
+import Sidebar from "../common/Sidebar";
 
 const AppLayout = () => {
-  return (
-    <div>
-        <Outlet />
-    </div>
-  )
-}
+	const navigate = useNavigate();
+	const [loading, setLoading] = useState(true);
 
-export default AppLayout
+	useEffect(() => {
+		const checkAuth = async () => {
+			const user = await authUtils.isAuthenticated();
+			if (!user) {
+				navigate("/login");
+			} else {
+				// save user
+				setLoading(false);
+			}
+		};
+		checkAuth();
+	}, [navigate]);
+
+	return loading ? (
+		<Loading fullHeight />
+	) : (
+		<Box
+			sx={{
+				display: "flex",
+			}}
+		>
+			<Sidebar />
+			<Box
+				sx={{
+					flexGrow: 1,
+					p: 1,
+					width: "max-content",
+				}}
+			>
+				<Outlet />
+			</Box>
+		</Box>
+	);
+};
+
+export default AppLayout;
